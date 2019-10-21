19f38cc547f7ffe720ba5ad3cb8dbf68ddcc0e15
channaveerayya <channucs0009@gmail.com>
2019-10-22 00:11:40 +0530

    Adding clients

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import './App.css';
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import AppNavBar from "./components/layout/AppNavBar";
 import Dashboard from './components/layout/Dashboard'
-
+import AddClient from './components/clients/AddClient';
 
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <AppNavBar />
-                <div className="container">
-                    <Switch>
-                        <Route exact path="/" component={Dashboard} />
-                    </Switch>
+        <Provider store={store}>
+            <Router>
+                <div className="App">
+                    <AppNavBar />
+                    <div className="container">
+                        <Switch>
+                            <Route exact path="/" component={Dashboard} />
+                            <Route exact path="/client/add" component={AddClient} />
+                        </Switch>
+                    </div>
                 </div>
-
-            </div>
-        </Router>
+            </Router>
+        </Provider>
     );
 }
 
--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,31 +1,29 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import Spinner from "../layout/Spinner";
 class Clients extends Component {
-	static propTypes = {
-		prop: PropTypes
+	state = {
+		totalOwed: null
 	};
 
+	static getDerivedStateFromProps(props, state) {
+		const { clients } = props;
+		if (clients) {
+			const total = clients.reduce((total, client) => {
+				return total + parseFloat(client.balance.toString());
+			}, 0);
+			return { totalOwed: total };
+		}
+
+		return null;
+	}
 	render() {
-		const clients = [
-			{
-				id: 1234,
-				firstName: "channu",
-				lastName: "BS",
-				email: "channucs009@gmail.com",
-				phone: "9980972009",
-				balance: "50"
-			},
-			{
-				id: 4444,
-				firstName: "abhi",
-				lastName: "Bellary",
-				email: "abhi@gmail.com",
-				phone: "994572009",
-				balance: "50"
-			}
-		];
+		const { clients } = this.props;
+		const { totalOwed } = this.state;
 
 		if (clients) {
 			return (
@@ -37,7 +35,14 @@ class Clients extends Component {
 								<i className="fas fa-users" /> Clients {""}
 							</h2>
 						</div>
-						<div className="col-md-6"></div>
+						<div className="col-md-6">
+							<h5 className="text-right text-secondary">
+								Total Owed
+								<span className="text-primary">
+									${parseFloat(totalOwed).toFixed(2)}
+								</span>
+							</h5>
+						</div>
 					</div>
 
 					<table className="table table-striped">
@@ -72,8 +77,18 @@ class Clients extends Component {
 				</div>
 			);
 		} else {
-			return <h1>Loading</h1>;
+			return <Spinner />;
 		}
 	}
 }
-export default Clients;
+
+Clients.propTypes = {
+	firestore: PropTypes.object.isRequired,
+	clients: PropTypes.array
+};
+export default compose(
+	firestoreConnect([{ collection: "clients" }]),
+	connect((state, props) => ({
+		clients: state.firestore.ordered.clients
+	}))
+)(Clients);
